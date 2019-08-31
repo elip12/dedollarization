6a1b240bf114b9a5610bc05d39c5faa05156fea9
Eli Pandolfo <epandolf@ucsc.edu>
2019-08-30 19:58:08 -0700

    first draft logic for randomly pairing people obeying probability rules, restructure some of the model fields

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
+import random
 
 
 class Constants(BaseConstants):
@@ -16,21 +17,64 @@ class Subsession(BaseSubsession):
         if self.round_number == 1:
             self.group_randomly()
 
+            # create random pairings
+            self.session.vars['pairs'] = []
+            for r in Constants.num_rounds:
+                pairs = {}
+
+                # a way to pair people given certain probabilities of
+                # getting paired within your group or within the other group
+                # NOTE: Constants.probability_of_same_group times
+                # Constants.players_per_group needs to be an integer.
+                g1 = [i for i in range(Constants.players_per_group)]
+                random.shuffle(g1)
+                g1_sample_homogeneous = random.sample(g1,
+                    Constants.players_per_group
+                    * Constants.probability_of_same_group)
+                g1_sample_heterogeneous = [x for x in g1
+                    if x not in g1_sample_homogeneous]
+                g2 = [i for i in range(Constants.players_per_group)]
+                random.shuffle(g2)
+                g2_sample_homogeneous = random.sample(g2,
+                    Constants.players_per_group
+                    * Constants.probability_of_same_group)
+                g2_sample_heterogeneous = [x for x in g2
+                    if x not in g2_sample_homogeneous]
+                for i in range(0, len(g1_sample_homogeneous), 2):
+                    pairs[(1, g1_sample_homogeneous[i])] = (1,
+                        g1_sample_homogeneous[i + 1])
+                    pairs[(1, g1_sample_homogeneous[i + 1])] = (1,
+                        g1_sample_homogeneous[i])
+                for i in range(0, len(g2_sample_homogeneous), 2):
+                    pairs[(2, g2_sample_homogeneous[i])] = (2,
+                        g2_sample_homogeneous[i + 1])
+                    pairs[(2, g2_sample_homogeneous[i + 1])] = (2,
+                        g2_sample_homogeneous[i])
+                for i in range(len(g1_sample_heterogeneous)):
+                    pairs[(1, g1_sample_heterogeneous[i])] = (2,
+                        g2_sample_heterogeneous[i])
+                    pairs[(2, g2_sample_heterogeneous[i])] = (1,
+                        g1_sample_heterogeneous[i])
+                self.session.vars['pairs'].append(pairs)
+                    
             # there will always only be 2 groups
             for g_index, g in enumerate(self.get_groups()):
+                # set group color for player
                 group_color = 'Red' if g_index == 0 else 'Blue'
+                # define random roles for players (producer/consumer),
+                # ensuring half are producers and half are consumers
                 roles = [None for n in range(Constants.players_per_group / 2)]
                 roles += [group_color for n in range(
                     Constants.players_per_group / 2)]
-                roles.shuffle()
+                random.shuffle(roles)
+                # set each player's group color, and starting token (which
+                # defines role
                 for p_index, p in enumerate(g.get_players()):
                     p.participant.vars['group_color'] = group_color
                     p.participant.vars['token'] = roles[p_index]
-                    if p.participant.vars['token']:
-                        p.participant.vars['role'] = 'Consumer'
-                    else:
-                        p.participant.vars['role'] = 'Producer'
-
+        else:
+            self.group_like_round(1)
+            
 class Group(BaseGroup):
     pass
 
--- pages.py
+++ pages.py
@@ -10,8 +10,23 @@ class Trade(Page):
     ]
 
     def vars_for_template(self):
+    '''
+        session.vars['pairs'] is a list of rounds.
+        each round is a dict of (group,id):(group,id) pairs.
+    '''
+        group_id = 0 if self.participant.vars['group_color'] == 'Red' else 1 
+        other_group, other_id = session.vars['pairs'][self.round_number][
+            (group_id, self.player.id_in_group)]
+        other_player = self.get_groups()[other_group].get_player_by_id(other_id)
+        other_token = other_player.partipant.vars['token']
+        other_group_color = other_player.participant.vars['group_color']
+
         return {
-            'role_pre': self.player.participant.vars['role']
+            'token_color': self.player.participant.vars['token'],
+            'group_color': self.player.participant.vars['group_color'],
+            'other_role_pre': other_role_pre,
+            'other_token_color': other_token,
+            'other_group_color': other_group_color,
         {
 
 class ResultsWaitPage(WaitPage):
@@ -25,8 +40,16 @@ class ResultsWaitPage(WaitPage):
     ]
     
     def vars_for_template(self):
+        group_id = 0 if self.participant.vars['group_color'] == 'Red' else 1 
+        other_group, other_id = session.vars['pairs'][self.round_number][
+            (group_id, self.player.id_in_group)]
+        other_player = self.get_groups()[other_group].get_player_by_id(other_id)
+        other_token = other_player.partipant.vars['token']
+        other_group_color = other_player.participant.vars['group_color']
+        
+        ''' handle the logic for switching tokens here '''
+
         return {
-           'role_pre': self.player.participant.vars['role'],
            'trade_attempted': self.player.trade_attempted,
            'initial_token_color': self.player.participant.vars['token_color'],
            'group_color': self.player.participant.vars['group_color'],
