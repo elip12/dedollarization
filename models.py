fbdb294701df57138b70692aed956d832febc1af
jokiboy1307 <54289302+jokiboy1307@users.noreply.github.com>
2019-09-04 02:01:26 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -6,13 +6,11 @@ import random
 
 
 class Constants(BaseConstants):
-    name_in_url = 'consumer_producer'
+    name_in_url = 'producer_consumer'
     players_per_group = 4
-    num_rounds = 8
+    num_rounds = 50
     endowment = c(50)
-    probability_of_same_group = .5
-    token_store_cost_homogeneous = 1
-    token_store_cost_heterogeneous = 2
+
 
 class Subsession(BaseSubsession):
     def creating_session(self):
@@ -26,43 +24,43 @@ class Subsession(BaseSubsession):
 
                 # a way to pair people given certain probabilities of
                 # getting paired within your group or within the other group
-                # NOTE: Constants.probability_of_same_group times
+                # NOTE: self.session.config['probability_of_same_group'] times
                 # Constants.players_per_group needs to be an integer.
                 g1 = [i for i in range(Constants.players_per_group)]
                 random.shuffle(g1)
                 g1_sample_homogeneous = random.sample(g1,
-                    int(Constants.players_per_group
-                    * Constants.probability_of_same_group))
+                                                      int(Constants.players_per_group
+                                                          * self.session.config['probability_of_same_group']))
                 g1_sample_heterogeneous = [x for x in g1
-                    if x not in g1_sample_homogeneous]
+                                           if x not in g1_sample_homogeneous]
                 g2 = [i for i in range(Constants.players_per_group)]
                 random.shuffle(g2)
                 g2_sample_homogeneous = random.sample(g2,
-                    int(Constants.players_per_group
-                    * Constants.probability_of_same_group))
+                                                      int(Constants.players_per_group
+                                                          * self.session.config['probability_of_same_group']))
                 g2_sample_heterogeneous = [x for x in g2
-                    if x not in g2_sample_homogeneous]
+                                           if x not in g2_sample_homogeneous]
                 for i in range(0, len(g1_sample_homogeneous), 2):
                     pairs[(0, g1_sample_homogeneous[i])] = (0,
-                        g1_sample_homogeneous[i + 1])
+                                                            g1_sample_homogeneous[i + 1])
                     pairs[(0, g1_sample_homogeneous[i + 1])] = (0,
-                        g1_sample_homogeneous[i])
+                                                                g1_sample_homogeneous[i])
                 for i in range(0, len(g2_sample_homogeneous), 2):
                     pairs[(1, g2_sample_homogeneous[i])] = (1,
-                        g2_sample_homogeneous[i + 1])
+                                                            g2_sample_homogeneous[i + 1])
                     pairs[(1, g2_sample_homogeneous[i + 1])] = (1,
-                        g2_sample_homogeneous[i])
+                                                                g2_sample_homogeneous[i])
                 for i in range(len(g1_sample_heterogeneous)):
                     pairs[(0, g1_sample_heterogeneous[i])] = (1,
-                        g2_sample_heterogeneous[i])
+                                                              g2_sample_heterogeneous[i])
                     pairs[(1, g2_sample_heterogeneous[i])] = (0,
-                        g1_sample_heterogeneous[i])
+                                                              g1_sample_heterogeneous[i])
                 self.session.vars['pairs'].append(pairs)
-                    
+
             # there will always only be 2 groups
             for g_index, g in enumerate(self.get_groups()):
                 # set group color for player
-                group_color = 'Red' if g_index == 0 else 'Blue'
+                group_color = 'Rojo' if g_index == 0 else 'Azul'
                 # define random roles for players (producer/consumer),
                 # ensuring half are producers and half are consumers
                 roles = ['None' for n in range(int(Constants.players_per_group / 2))]
@@ -74,29 +72,28 @@ class Subsession(BaseSubsession):
                 for p_index, p in enumerate(g.get_players()):
                     p.participant.vars['group_color'] = group_color
                     p.participant.vars['token'] = roles[p_index]
+                    p.participant.payoff += Constants.endowment
         else:
             self.group_like_round(1)
-            
+
+
 class Group(BaseGroup):
     pass
 
+
 class Player(BasePlayer):
-    role_pre = models.StringField() # 'Producer', 'Consumer'
+    role_pre = models.StringField()  # 'Producer', 'Consumer'
     other_role_pre = models.StringField()
-    token_color = models.StringField() # 'Red', 'Blue', None
-    group_color = models.StringField() # 'Red', 'Blue'
+    token_color = models.StringField()  # 'Red', 'Blue', None
+    group_color = models.StringField()  # 'Red', 'Blue'
     other_group_color = models.StringField()
     trade_attempted = models.BooleanField(
         choices=[
             [False, 'No'],
-            [True, 'Yes'],
+            [True, 'Sí'],
         ]
     )
     trade_succeeded = models.BooleanField()
 
-    def set_payoffs(self, round_payoff, token_color):
+    def set_payoffs(self, round_payoff):
         self.payoff = round_payoff
-        if self.participant.vars['group_color'] == token_color:
-            self.payoff -= c(Constants.token_store_cost_homogeneous)
-        elif token_color != 'None':
-            self.payoff -= c(Constants.token_store_cost_heterogeneous)
--- pages.py
+++ pages.py
@@ -2,33 +2,35 @@ from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
 
+
 # Description of the game: How to play and returns expected
 class Introduction(Page):
     def is_displayed(self):
         return self.round_number == 1
 
+
 class Trade(Page):
     timeout_seconds = 30
     form_model = 'player'
     form_fields = [
-        'role_pre',
-        'other_role_pre',
-        'group_color',
-        'other_group_color',
         'trade_attempted',
     ]
 
     def vars_for_template(self):
         # self.session.vars['pairs'] is a list of rounds.
         # each round is a dict of (group,id):(group,id) pairs.
-        group_id = 0 if self.participant.vars['group_color'] == 'Red' else 1 
+        group_id = 0 if self.participant.vars['group_color'] == 'Rojo' else 1
         other_group, other_id = self.session.vars['pairs'][self.round_number - 1][
             (group_id, self.player.id_in_group - 1)]
         other_player = self.subsession.get_groups()[other_group].get_player_by_id(other_id + 1)
         other_token = other_player.participant.vars['token']
         other_group_color = other_player.participant.vars['group_color']
-        role_pre = 'Consumer' if self.player.participant.vars['token'] != 'None' else 'Producer'
-        other_role_pre = 'Consumer' if other_token != 'None' else 'Producer'
+        role_pre = 'Comprador' if self.player.participant.vars['token'] != 'None' else 'Vendedor'
+        other_role_pre = 'Comprador' if other_token != 'None' else 'Vendedor'
+        self.player.role_pre = role_pre
+        self.player.other_role_pre = other_role_pre
+        self.player.group_color = self.player.participant.vars['group_color']
+        self.player.other_group_color = other_group_color
         return {
             'role_pre': role_pre,
             'other_role_pre': other_role_pre,
@@ -38,42 +40,35 @@ class Trade(Page):
             'other_group_color': other_group_color,
         }
 
-
     def before_next_page(self):
         if self.timeout_happened:
             self.player.trade_attempted = False
 
+
 class ResultsWaitPage(WaitPage):
     body_text = 'Waiting for other participants to decide.'
     wait_for_all_groups = True
+
     def after_all_players_arrive(self):
         pass
 
+
 class Results(Page):
     timeout_seconds = 30
-    form_model = 'player'
-    form_fields = [
-        'token_color',
-        'trade_succeeded',
-    ]
-    
+
     def vars_for_template(self):
-        group_id = 0 if self.player.participant.vars['group_color'] == 'Red' else 1 
+        group_id = 0 if self.player.participant.vars['group_color'] == 'Rojo' else 1
         other_group, other_id = self.session.vars['pairs'][self.round_number - 1][
             (group_id, self.player.id_in_group - 1)]
         other_player = self.subsession.get_groups()[other_group].get_player_by_id(other_id + 1)
         other_token = other_player.participant.vars['token']
         other_group_color = other_player.participant.vars['group_color']
-        
         round_payoff = c(0)
-
         # switching tokens
         initial_token_color = self.player.participant.vars['token']
         if self.player.trade_attempted and other_player.trade_attempted:
-            
             self.player.participant.vars['token'] = other_token
             other_player.participant.vars['token'] = initial_token_color
-            
             trade_succeeded = True
             if initial_token_color != 'None':
                 round_payoff = c(20)
@@ -81,16 +76,23 @@ class Results(Page):
             trade_succeeded = False
         new_token_color = self.player.participant.vars['token']
         if initial_token_color != 'None':
-            role_pre = 'Consumer'
+            role_pre = 'Comprador'
         else:
-            role_pre = 'Producer'
+            role_pre = 'Vendedor'
         if other_token != 'None':
-            other_role_pre = 'Consumer'
+            other_role_pre = 'Comprador'
         else:
-            other_role_pre = 'Producer'
+            other_role_pre = 'Vendedor'
         token_color = initial_token_color if initial_token_color != 'None' \
             else new_token_color
-        self.player.set_payoffs(round_payoff, new_token_color)
+        # penalties
+        if self.participant.vars['group_color'] == new_token_color:
+            round_payoff -= c(self.session.config['token_store_cost_homogeneous'])
+        elif token_color != 'None':
+            round_payoff -= c(self.session.config['token_store_cost_heterogeneous'])
+        self.player.set_payoffs(round_payoff)
+        self.player.token_color = token_color
+        self.player.trade_succeeded = trade_succeeded
         return {
             'token_color': token_color,
             'role_pre': role_pre,
@@ -110,4 +112,3 @@ page_sequence = [
     ResultsWaitPage,
     Results
 ]
-
